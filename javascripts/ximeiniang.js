8d080ac8e4e0a34775369eb13a7b3c73d248d80d
lisnb <lisnb.h@gmail.com>
2016-09-10 19:36:43 +0800

    update to fullpage

---
+++ javascripts/ximeiniang.js
@@ -0,0 +1,48 @@
+/**
+* @Author: lisnb
+* @Date:   2016-09-10T17:58:16+08:00
+* @Email:  lisnb.h@hotmail.com
+* @Last modified by:   lisnb
+* @Last modified time: 2016-09-10T18:50:40+08:00
+*/
+(function($) {
+$(document).ready(function() {
+  var dbpath = './db/dishes.json'
+  var tmpl_single_dish = $.templates('#tmpl-single-dish')
+  var tmpl_single_slide = $.templates('#tmpl-single-slide')
+  var tmpl_section = $.templates('#tmpl-section')
+  function getDBData(callback) {
+    $.get(dbpath, function(data){
+      console.log(data);
+      console.log(typeof(data))
+      // var dishes = JSON.parse(data);
+      callback(data['dishes'])
+    })
+  }
+  function initFullpage(dishes) {
+    var dishesArray = []
+    var currentDishHTML = ''
+    $.each(dishes, function(index, dish) {
+      console.log(dish)
+      if (dish['multislide']) {
+        currentDishHTML = tmpl_single_slide.render(dish['slides'])
+      } else {
+        currentDishHTML = tmpl_single_dish.render(dish['slide'])
+      }
+      dishesArray.push({
+        'slides': currentDishHTML
+      })
+    })
+    console.log(dishesArray)
+    var fullpageHTML = tmpl_section.render(dishesArray)
+    console.log(fullpageHTML)
+    $('#fullpage').html(fullpageHTML)
+    $('#fullpage').fullpage()
+  }
+  function init() {
+    getDBData(initFullpage);
+  }
+
+  init();
+})
+})(jQuery)
